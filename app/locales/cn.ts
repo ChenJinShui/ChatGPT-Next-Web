b39c41d9f5d98a7b2411eab3d8e2bafc5839efba
ChenJinShui <42457310+ChenJinShui@users.noreply.github.com>
2024-01-15 17:23:58 +0800

    Update cn.ts

--- app/locales/cn.ts
+++ app/locales/cn.ts
@@ -359,7 +359,7 @@ const cn = {
   },
   Store: {
     DefaultTopic: "新的聊天",
-    BotHello: "有什么可以帮你的吗",
+    BotHello: "本项目已不再继续维护了，请跳转新的链接 http://chatgpt.chenjinshui.top/shared.html，祝大家身体健康，工作顺利",
     Error: "出错了，稍后重试吧",
     Prompt: {
       History: (content: string) => "这是历史聊天总结作为前情提要：" + content,
