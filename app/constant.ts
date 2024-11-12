38b0d2d936816c4fac005b4ccf55e47ab1e8a593
ChenJinShui <42457310+ChenJinShui@users.noreply.github.com>
2024-11-12 09:19:28 +0800

    Update constant.ts

--- app/constant.ts
+++ app/constant.ts
@@ -561,5 +561,5 @@ export const PLUGINS = [
   { name: "Search Chat", path: Path.SearchChat },
 ];
 
-export const SAAS_CHAT_URL = "https://nextchat.dev/chat";
-export const SAAS_CHAT_UTM_URL = "https://nextchat.dev/chat?utm=github";
+export const SAAS_CHAT_URL = "https://gpt35.chenjinshui.top/";
+export const SAAS_CHAT_UTM_URL = "https://gpt35.chenjinshui.top/";
